598b27496efac0992462f6c33310ab308dc82027
jogalt <49085678+jogalt@users.noreply.github.com>
2023-09-05 11:56:06 -0400

    Update OpenXPKI_Installer_v23.09.05.003.sh

--- OpenXPKI_Installer_v23.09.05.003.sh
+++ OpenXPKI_Installer_v23.09.05.003.sh
@@ -1126,12 +1126,8 @@ read v_new_user
 echo "Enter user password."
 echo ""
 read v_new_user_pass
-salt="$(openssl rand -base64 3)"
-#password="secretpassword"
-#$(echo -n $(echo -n "$password$salt" | openssl sha1 -binary)$salt | openssl enc -base64)
-#salt=`openssl rand -base64 3`
+salt=$(openssl rand -base64 3)
 echo $salt
-#v_new_user_saltPass=`echo -n "$v_new_user_pass$salt" | openssl sha1 -binary` # )$salt` | openssl enc -base64`
 v_new_user_saltPass=$(echo -n $(echo -n "$password$salt" | openssl sha1 -binary)$salt | openssl enc -base64)
 echo $v_new_user_saltPass
 # Add new user details to the userdb or admindb
